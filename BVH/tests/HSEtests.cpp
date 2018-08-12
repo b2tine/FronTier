81489aa8cba39f58cc907a0def17f4e0034f1a95
Brandon <brandon.ballentine@stonybrook.edu>
2018-08-12 01:50:35 -0400

    Added preliminary test of constructor FT_POINT.

--- BVH/tests/HSEtests.cpp
+++ BVH/tests/HSEtests.cpp
@@ -1,3 +1,24 @@
 #include "gmock/gmock.h"
+#include "FronTier.h"
+
+//Base class for hypersurface elements (HSE).
+//Can refer to a point, bond, or triangle
+//depending on the dimension of the hypersurface.
+class FT_HSE
+{
+    public:
+        ~FT_HSE() = default;
+};
+
+class FT_POINT
+    : public FT_HSE
+{
+    public:
+};
 
 
+
+TEST(FT_HSE, ReturnNumberOfPointsInHSE)
+{
+    FT_POINT ft_point;
+}
